f39780be2bd6730c665c6f5be0d7bb311ee8c732
Pavel Lobanov <pslobanov@avito.ru>
2024-11-30 16:16:55 +0300

    Add Exc5

---
+++ Exc5/run_services.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+kubectl run front-end-app --image=nginx --labels role=front-end --expose --port 80 -n default
+kubectl run back-end-api-app --image=nginx --labels role=back-end-api --expose --port 80  -n default
+kubectl run admin-front-end-app --image=nginx --labels role=admin-front-end --expose --port 80  -n default
+kubectl run admin-back-end-api-app --image=nginx --labels role=admin-back-end-api --expose --port 80  -n default
